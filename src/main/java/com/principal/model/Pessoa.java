ec72e5733f38f518f2e2cf263c1cac93a3bf3c76
Rodrigo Fernandes <rodrigofernandes3@hotmail.com>
2023-01-18 10:37:42 -0300

    Subindo projeto

---
+++ src/main/java/com/principal/model/Pessoa.java
@@ -0,0 +1,37 @@
+package com.principal.model;
+
+import java.util.Date;
+
+public class Pessoa {
+	
+	private String nome;
+	private String email;
+	private String cpf;
+	private Date dtNascimento;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Date getDtNascimento() {
+		return dtNascimento;
+	}
+	public void setDtNascimento(Date dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+	
+}
